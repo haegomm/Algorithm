85b9f6da2ee7d5133df0c2c97006950b03b72d47
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-09-12 13:24:08 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 162 ms, Memory: 58,780 KB -BaekjoonHub

---
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for tc in range(1, t + 1):
+    p, q, r, s, w = map(int, input().split())
+    a = w * p
+    if w < r:
+        b = q
+    else:
+        b = q + ((w - r) * s)
+    if a < b:
+        print(f"#{tc}", a)
+    else:
+        print(f"#{tc}", b)
