285d1b132771bf045c96d74404b70cc07fec283a
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-09-16 10:45:21 +0900

    [D3] Title: 세제곱근을 찾아라, Time: 1,222 ms, Memory: 60,084 KB -BaekjoonHub

---
+++ SWEA/D3/5688. 세제곱근을 찾아라/세제곱근을 찾아라.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for tc in range(1, t + 1):
+    n = int(input())
+    result = -1
+    for i in range(1, 10**18):
+        if i**3 > n:
+            break
+        elif n % i == 0 and n == i ** 3:
+            result = i
+            break
+
+    print(f'#{tc}', result)
