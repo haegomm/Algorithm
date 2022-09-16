3e2b07668894b671ee709e413be716773f9b6a73
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-09-16 14:52:29 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 625 ms, Memory: 191,896 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -6,15 +6,13 @@ for tc in range(1, t + 1):
     n, top = map(int, input().split())
     arr = list(map(int, input().split()))
     subsets = []
-    m = 1000000000
+    s = []
 
     for i in range(len(arr) + 1):
-        subsets = subsets + list(combinations(arr, i))
+        subsets = list(combinations(arr, i))
         for i in subsets:
             check = sum(i)
             if check >= top:
-                result = check - top
-                if result < m:
-                    m = result
-                    
-    print(f'#{tc}', m)
+                s.append(check)
+
+    print(f'#{tc}', min(s) - top)
