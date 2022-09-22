b614724c0e7c9971ff79c2863118a01e4e3a7d8d
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-09-22 16:04:47 +0900

    [Silver I] Title: 회의실 배정, Time: 4356 ms, Memory: 51616 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,17 +1,15 @@
-import sys
-n = int(sys.stdin.readline())
-time = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
-time.sort(key=lambda x: (x[1], x[0]))
+n = int(input())
+time_line = [list(map(int, input().split())) for _ in range(n)]
 
-i = 0
-j = 1
-cnt = 1
-while j < n:
-    if time[i][1] <= time[j][0]:
-        cnt += 1
-        i = j
-        j += 1
-    else:
-        j += 1
+time_line.sort(key=lambda x: x[0])
+time_line.sort(key=lambda x: x[1])
 
-print(cnt)
+answer = 0
+end = 0
+
+for i in range(n):
+    if time_line[i][0] >= end:
+        end = time_line[i][1]
+        answer += 1
+
+print(answer)
