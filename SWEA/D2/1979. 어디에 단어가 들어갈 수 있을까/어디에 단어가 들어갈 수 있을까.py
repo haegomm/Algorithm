b65ac339bc395be928d65166d7587c23de7fd2dd
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-09-25 22:12:26 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 164 ms, Memory: 58,792 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,21 +1,14 @@
-t = int(input())
-
-for tc in range(1, t + 1):
-
+for t in range(1, int(input()) + 1):
     n, k = map(int, input().split())
+    arr_row = [input().replace(" ", "") for _ in range(n)]
+    arr_col = list(map(''.join, zip(*arr_row)))
+    cnt = 0
 
-    arr_row = [list(map(int, input().split())) for _ in range(n)]
-    arr_col = list(map(list, zip(*arr_row)))
-
-    result = 0
     for arr in arr_row, arr_col:
         for line in arr:
-            for i in range(n):
-                if line[i] == 1:
-                    word_line = sum(line[i:i+k])
-                    if word_line == k:  # 1 1 1 1 0 1 1 1
-                        if i + k == n or ((i+k < n) and line[i+k] == 0):
-                            if i == 0 or (i - 1 >= 0 and line[i-1] == 0):
-                                result += 1
+            real = list(line.split('0'))
+            for check in real:
+                if check == ('1' * k):
+                    cnt += 1
 
-    print(f'#{tc} {result}')
+    print(f'#{t}', cnt)
