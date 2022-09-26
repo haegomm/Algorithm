05e2f0c0d600ea96a0ceea431a224b0a33dd4ecf
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-09-26 13:10:04 +0900

    [Silver V] Title: 수 이어가기, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2635. 수 이어가기/수 이어가기.py
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.py
@@ -1,18 +1,17 @@
 n = int(input())
+result = []
 
-result = [n]
-
-for n2 in range(n, 0, -1):
-    numbers = [n, n2]
-    n1 = n
-    for _ in range(n+1):
-        n3 = n1 - n2
-        if n3 >= 0:
-            numbers.append(n3)
+for num2 in range(n//2, n+1):
+    numbers = [n, num2]
+    num1 = n
+    while True:
+        num3 = num1 - num2
+        if num3 >= 0:
+            numbers.append(num3)
+            num1 = num2
+            num2 = num3
         else:
             break
-        n1 = n2
-        n2 = n3
 
     if len(numbers) > len(result):
         result = numbers
