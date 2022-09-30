d0db57d815d7a1cba9e6ae7f5e8cdcff2ce16259
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-09-30 10:16:37 +0900

    [Gold IV] Title: 네트워크 연결, Time: 4492 ms, Memory: 52488 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,35 +1,33 @@
-import sys
-input = sys.stdin.readline
+def prim(start):
+    visited = [False] * (n + 1)
+    distance = [inf] * (n + 1)
+    distance[start] = 0
+    cost= 0
 
-def find_set(node):
-    if node != parent[node]:
-        parent[node] = find_set(parent[node])
-    return parent[node]
+    for _ in range(n):
+        min_dist = inf
+        for i, dist in enumerate(distance):
+            if not visited[i] and dist < min_dist:
+                min_node = i
+                min_dist = dist
+
+        visited[min_node] = True
+        cost += min_dist
+
+        for next_node, dist in graph[min_node]:
+            if not visited[next_node] and dist < distance[next_node]:
+                distance[next_node] = dist
+    return cost
 
 
-computer_cnt = int(input())
-edges_cnt = int(input())
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+inf = 99999999
 
-parent = list(range(computer_cnt + 1))
+for _ in range(m):
+    s, e, w = map(int, input().split())
+    graph[s].append((e, w))
+    graph[e].append((s, w))
 
-edges = []
-for _ in range(edges_cnt):
-    a, b, cost = map(int, input().split())
-    edges.append((cost, a, b))
-
-edges.sort()        # 리스트 안 튜플은 맨앞 cost를 기준으로 정렬한다! 밑에서 작은 cost부터 불러오게 됨
-connect_cnt = 0     # 연결횟수
-cost_sum = 0        # cost합
-
-for cost, a, b in edges:
-    a_root, b_root = find_set(a), find_set(b)   # 대표원소 찾기
-
-    if a_root != b_root:                        # 루트가 다르다 == 다른 집합이다(서로소) == 컴퓨터 연결이 아직 안되어있다!
-        parent[b_root] = a_root                 # 합쳐주고
-        connect_cnt += 1                        # 연결수 +1
-        cost_sum += cost                        # cost 더해주기
-        
-        if connect_cnt >= computer_cnt - 1:     # 컴퓨터 개수 - 1 == 연결횟수 가 가장 적게 연결되면서 전부 연결되는 경우이다!
-            break
-        
-print(cost_sum)
+print(prim(1))
