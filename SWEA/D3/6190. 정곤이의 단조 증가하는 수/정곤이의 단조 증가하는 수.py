43eabc592e0e65ce76e1ebf2c63daeead86a30da
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-10-10 10:53:17 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 834 ms, Memory: 63,580 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
@@ -1,21 +1,17 @@
-t = int(input())
-
-for tc in range(1, t + 1):
-
+for t in range(1, int(input()) + 1):
     n = int(input())
-    numbers = list(map(int, input().split()))
+    numbers = sorted(list(map(int, input().split())), reverse=True)
+    result = -1
 
-    max_ij = -1
     for i in range(n-1):
         for j in range(i+1, n):
-            mul_num = numbers[i] * numbers[j]
-            if mul_num >= 10:
-                mul_num_str = list(str(mul_num))
-                for k in range(len(mul_num_str) - 1):
-                    if mul_num_str[k] > mul_num_str[k+1]:
-                        break
-                else:
-                    if max_ij < mul_num:
-                        max_ij = mul_num
+            mul = str(numbers[i] * numbers[j])
+            for j in range(len(mul)-1):
+                if mul[j] > mul[j+1]:
+                    break
+            else:
+                if int(result) < int(mul):
+                    result = mul
+                    break
 
-    print(f'#{tc} {max_ij}')
+    print(f'#{t} {result}')
