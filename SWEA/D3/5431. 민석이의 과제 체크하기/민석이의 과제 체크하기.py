7ab1c6c22d1f9e2c034392b6c5cd50353eb9dc49
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-10-10 12:44:13 +0900

    [D3] Title: 민석이의 과제 체크하기, Time: 363 ms, Memory: 64,624 KB -BaekjoonHub

--- SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
+++ SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
@@ -1,15 +1,6 @@
-t = int(input())
-
-for tc in range(1, t + 1):
-
-    n, m = map(int, input().split())
-    no_task_num = list(map(int, input().split()))
-
-    student = []
-    for i in range(1, n + 1):
-        if i not in no_task_num:
-            student.append(i)
-
-    yes_task_student = ' '.join(map(str, student))
-
-    print(f'#{tc} {yes_task_student}')
+for t in range(1, int(input()) + 1):
+    n, k = map(int, input().split())
+    s = [num for num in range(1, n+1)]
+    for y in map(int, input().split()):
+        s.remove(y)
+    print(f'#{t}', *s)
