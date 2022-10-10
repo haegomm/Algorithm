13f9f5bd0f05255bd57e2f9f62d4fd0fdeabbf8c
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-10-10 12:48:51 +0900

    [D3] Title: 민석이의 과제 체크하기, Time: 349 ms, Memory: 64,900 KB -BaekjoonHub

--- SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
+++ SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
@@ -1,6 +1,8 @@
 for t in range(1, int(input()) + 1):
     n, k = map(int, input().split())
-    s = [num for num in range(1, n+1)]
+    s = list(range(1, n+1))
+    
     for y in map(int, input().split()):
         s.remove(y)
+
     print(f'#{t}', *s)
