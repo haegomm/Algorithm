9c7f9764bbbca7802cb3070bf6ff4861c572b5a9
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-10-15 18:37:18 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,6 +1,6 @@
 def solution(lottos, win_nums):
     rank = [6, 6, 5, 4, 3, 2, 1]
     same = len(set(lottos) & set(win_nums))
-    zeo = len(list(filter(lambda x: x == 0, lottos)))
+    zeo = lottos.count(0)
     answer = [rank[same+zeo], rank[same]]
     return answer
