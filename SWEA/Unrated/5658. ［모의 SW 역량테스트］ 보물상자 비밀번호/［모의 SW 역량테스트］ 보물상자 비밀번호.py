f8e97513ca0708aba9ab5a5b50a7433ba6696b37
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-10-20 20:21:25 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 149 ms, Memory: 58,524 KB -BaekjoonHub

---
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.py
@@ -0,0 +1,15 @@
+for t in range(1, int(input()) + 1):
+    n, k = map(int, input().split())
+    password = input()
+    unlock = []
+    turn = n // 4
+
+    for _ in range(turn):
+        for i in range(0, n - turn + 1, turn):
+            unlock.append(int(password[i:i + turn], 16))
+
+        password = password[-1] + password[:n-1]
+
+    result = sorted(set(unlock), reverse=True)
+
+    print(f'#{t} {result[k-1]}')
