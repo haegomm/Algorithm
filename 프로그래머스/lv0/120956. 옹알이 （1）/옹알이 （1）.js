b6935fc116ec015628e6e240921f56c1d76269f3
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2022-11-05 20:19:48 +0900

    [level 0] Title: 옹알이 (1), Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,10 @@
+function solution(babbling) {
+    let counter = 0;
+    babbling.forEach(element => {
+        if (element.split(/aya|ye|woo|ma/g).join('') === '') {
+            counter++;
+        }
+    });
+    return counter;
+    return counter;
+}
