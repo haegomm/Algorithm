8391eba7827155e1c4e5d5b0c5e2a26e0aa2b907
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-03-06 21:05:21 +0900

    [unrated] Title: 무인도 여행, Time: 0.90 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
@@ -1,35 +1,38 @@
-from collections import deque
-dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
-
-def island(maps, i, j, n, m, visit):
-    visit[i][j] = 1
-    q = deque()
-    q.append([i, j])
-    days = 0
-    while q:
-        i, j = q.popleft()
-        days += int(maps[i][j])
-        for d in range(4):
-            x, y = i + dx[d], j + dy[d]
-            if not(0 <= x < n and 0 <= y < m): continue
-            if visit[x][y] == 0 and maps[x][y] != 'X':
-                q.append([x, y])
-                visit[x][y] = 1
-    return days
+import sys
+sys.setrecursionlimit(10000)
 
 def solution(maps):
+
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    def bfs(x, y):
+        nonlocal survival
+        survival += int(maps[x][y])
+        maps[x][y] = "X"
+
+        for d in range(4):
+            next_x = x + dx[d]
+            next_y = y + dy[d]
+            if 0 <= next_x < n and 0 <= next_y < m and maps[next_x][next_y] != "X":
+                bfs(next_x, next_y)
+
     answer = []
+    n = len(maps)  # 행
+    m = len(maps[0])  # 열
+
     for i in range(len(maps)):
         maps[i] = list(maps[i])
-    n = len(maps)
-    m = len(maps[0])
-    visit = [[0] * m for _ in range(n)]
-    
+
     for i in range(n):
         for j in range(m):
-            if maps[i][j] != 'X' and visit[i][j] == 0:
-                answer.append(island(maps, i, j, n, m, visit))
+            if maps[i][j] != "X":
+                survival = 0
+                bfs(i, j)
+                answer.append(survival)
+
     if answer:
         return sorted(answer)
+
     else:
         return [-1]
