f32ad33e4f16dd9a3b0893059c5ffda838dfc7b3
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-03-13 20:27:35 +0900

    [unrated] Title: 덧칠하기, Time: 1.11 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -1,10 +1,10 @@
 def solution(n, m, section):
     answer = 0
     last_paint_num = 0
-    for one_more in section:
-        if one_more <= last_paint_num:
+    for start_paint_num in section:
+        if start_paint_num <= last_paint_num:
             continue
         else:
             answer += 1
-            last_paint_num = one_more + m - 1
+            last_paint_num = start_paint_num + m - 1
     return answer
