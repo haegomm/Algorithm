47af9722593f927103906ae813c236958f5767e8
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-07-04 20:24:01 +0900

    [Silver III] Title: 프린터 큐, Time: 68 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,21 +1,23 @@
+import sys
 from collections import deque
+import heapq
 
-for t in range(int(input())):
-    n, m = map(int, input().split())
-    document = deque(list(map(int, input().split())))
+tc = int(sys.stdin.readline())
+
+for _ in range(tc):
+    n, m = map(int, sys.stdin.readline().split())
+    documents = deque(enumerate(map(int, sys.stdin.readline().split())))
+    priority_queue = []
+    for idx, priority in documents:
+        heapq.heappush(priority_queue, -priority)
     cnt = 0
-    while document:
-        best = max(document)
-        now = document.popleft()
-        m -= 1
-
-        if best == now:
+    while documents:
+        idx, priority = documents.popleft()
+        if priority == -priority_queue[0]:
+            heapq.heappop(priority_queue)
             cnt += 1
-            if m < 0:
+            if idx == m:
                 print(cnt)
                 break
-
         else:
-            document.append(now)
-            if m < 0:
-                m = len(document) - 1
+            documents.append((idx, priority))
