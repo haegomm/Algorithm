f634048083bcd6ada75bad9fa03244f5c0a087fb
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-07-13 16:02:22 +0900

    [Gold V] Title: 탑, Time: 376 ms, Memory: 221928 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -4,13 +4,11 @@ stack = []
 answer = []
 
 for i in range(n):
-
     while stack and stack[-1][1] < tops[i]:
         stack.pop()
 
     if not stack:
         answer.append(0)
-
     else:
         answer.append(stack[-1][0] + 1)
 
