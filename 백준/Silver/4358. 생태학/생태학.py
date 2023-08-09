c75646f51ea1d0ab572a163f12585872884a9701
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-08-09 14:57:34 +0900

    [Silver II] Title: 생태학, Time: 244 ms, Memory: 117152 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.py
+++ 백준/Silver/4358. 생태학/생태학.py
@@ -1,18 +1,8 @@
 import sys
+from collections import Counter
 
-trees = {}
-total = 0
-
-while True:
-    tree = sys.stdin.readline().rstrip()
-    if not tree:
-        break
-    if not trees.get(tree):
-        trees[tree] = 1
-    else:
-        trees[tree] += 1
-    total += 1
-
-ordered_trees = sorted(trees.items())
-for tree, num in ordered_trees:
-    print(f'{tree} {num/total*100:.4f}')
+trees = Counter(sys.stdin.read().split("\n"))
+del trees[""]
+total = sum(trees.values())
+for tree, num in sorted(trees.items()):
+    print(f"{tree} {100 * num / total:.4f}")
