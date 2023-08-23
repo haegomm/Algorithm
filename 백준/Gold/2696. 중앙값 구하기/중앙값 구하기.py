6c9a7c2cf3246ebab1ed4a342053ef5917eb8a5a
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-08-23 20:52:22 +0900

    [Gold II] Title: 중앙값 구하기, Time: 56 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -1,19 +1,33 @@
 import sys
+from heapq import heappush, heappop
 
 input = sys.stdin.readline
 
-tc = int(input())
-for _ in range(tc):
+for _ in range(int(input())):
+    m = int(input())
+    nums = []
+    for _ in range(m // 10 + (m % 10 > 0)):
+        nums.extend(map(int, input().split()))
+    max_heap = []
+    min_heap = []
     answer = []
-    t = int(input().strip())
-    numbers = []
-    for _ in range((t + 9) // 10):
-        numbers.extend(map(int, input().split()))
-    for idx, num in enumerate(numbers):
-        idx += 1
-        if idx % 2 != 0:
-            temp = sorted(numbers[:idx])
-            answer.append(temp[(idx - 1) // 2])
-    print(len(answer))
-    for i in range(0, len(answer), 10):
-        print(*answer[i : i + 10])
+    for i in range(m):
+        if i % 2 == 0:
+            if not min_heap or min_heap[0] >= nums[i]:
+                heappush(max_heap, (-nums[i]))
+            else:
+                heappush(min_heap, nums[i])
+                heappush(max_heap, -heappop(min_heap))
+            answer.append(-max_heap[0])
+        else:
+            if -max_heap[0] <= nums[i]:
+                heappush(min_heap, nums[i])
+            else:
+                heappush(max_heap, -nums[i])
+                heappush(min_heap, -heappop(max_heap))
+    out_len = len(answer)
+    print(out_len)
+    for i in range(out_len // 10):
+        print(*answer[i * 10 : (i + 1) * 10])
+    if out_len % 10 > 0:
+        print(*answer[out_len // 10 * 10 :])
