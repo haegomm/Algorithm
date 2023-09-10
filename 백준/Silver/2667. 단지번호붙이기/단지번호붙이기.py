21580dc4b2995d9709ff25fc0b7c8008faff825f
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-09-10 14:28:22 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31332 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -5,14 +5,14 @@ input = sys.stdin.readline
 
 def dfs(x, y):
     global total
-    visited[x][y] = True
+    arr[x][y] = 0
     total += 1
 
     for k in range(4):
         nx = x + dx[k]
         ny = y + dy[k]
 
-        if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and arr[nx][ny] == 1:
+        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny]:
             dfs(nx, ny)
 
 
@@ -21,12 +21,11 @@ dy = [0, 0, -1, 1]
 
 n = int(input().strip())
 arr = [list(map(int, input().strip())) for _ in range(n)]
-visited = [[False] * n for _ in range(n)]
 result = []
 
 for i in range(n):
     for j in range(n):
-        if not visited[i][j] and arr[i][j] == 1:
+        if arr[i][j]:
             total = 0
             dfs(i, j)
             result.append(total)
