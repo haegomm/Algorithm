a00fe1d158636a29d25f1c899d3e900dfc9c0807
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-09-13 13:36:13 +0900

    [Silver I] Title: 봄버맨, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.py
+++ 백준/Silver/16918. 봄버맨/봄버맨.py
@@ -3,15 +3,18 @@ import sys
 input = sys.stdin.readline
 
 
-def boom(x, y):
-    temp[x][y] = '.'
-
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-
-        if 0 <= nx < r and 0 <= ny < c and temp[nx][ny] != '.':
-            temp[nx][ny] = '.'
+def boom(board):
+    temp = [['O'] * c for _ in range(r)]
+    for x in range(r):
+        for y in range(c):
+            if board[x][y] == 'O':
+                temp[x][y] = '.'
+                for k in range(4):
+                    nx = x + dx[k]
+                    ny = y + dy[k]
+                    if 0 <= nx < r and 0 <= ny < c:
+                        temp[nx][ny] = '.'
+    return temp
 
 
 dx = [-1, 1, 0, 0]
@@ -19,21 +22,23 @@ dy = [0, 0, -1, 1]
 
 r, c, n = map(int, input().split())
 board = [list(input().strip()) for _ in range(r)]
-time = 0
+time = 1
 
-if n % 2 == 0:
+if n == 1:
+    for li in board:
+        print(''.join(li))
+
+elif n % 2 == 0:
     for _ in range(r):
         print('O' * c)
-else:
-    time = 1
-    while time < n:
-        temp = [['O'] * c for _ in range(r)]
-        for i in range(r):
-            for j in range(c):
-                if board[i][j] == 'O':
-                    boom(i, j)
-        else:
-            board = temp
-            time += 2
-    for row in board:
+
+elif n % 4 == 3:
+    result = boom(board)
+    for li in result:
+        print(''.join(li))
+
+elif n % 4 == 1:
+    result = boom(board)
+    result = boom(result)
+    for row in result:
         print(''.join(row))
