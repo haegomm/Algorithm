5b26c579f1ed2e1ec7729540af9993ed26af59c5
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-09-29 10:34:53 +0900

    [Gold V] Title: ABCDE, Time: 792 ms, Memory: 243868 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,37 +1,36 @@
-# dfs
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 sys.setrecursionlimit(100000)
 
-n, m = map(int , input().split())
-visited = [False]*(n)
-adjacent = [ [] for _ in range(n) ]
-arrive = False
+
+def dfs(v, d):
+    global result
+    visited[v] = True
+
+    if d == 4:
+        result = 1
+        return
+
+    for next_v in graph[v]:
+        if not visited[next_v]:
+            dfs(next_v, d + 1)
+    visited[v] = False
+
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
+visited = [False] * n
+result = 0
 
 for _ in range(m):
-    a,b = map(int, input().split())
-    adjacent[a].append(b)
-    adjacent[b].append(a)
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-def dfs(start , depth):
-    global arrive
-    visited[start]=True
-    if depth==5:
-        arrive = True
-        return
-    for i in adjacent[start]:
-        if visited[i] == False:
-            dfs(i , depth+1)
-    visited[start]=False
-
-for i in range(n):
-    dfs(i ,1)
-    if arrive:
+for now in range(n):
+    dfs(now, 0)
+    if result == 1:
         break
 
-if arrive:
-    print(1)
-else:
-    print(0)
+print(result)
