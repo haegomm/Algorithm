2cb3a0be7a40f9555ea0921728aca29e93feed9b
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-10-12 20:53:27 +0900

    [Gold IV] Title: 연구소, Time: 2296 ms, Memory: 34116 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,45 +1,38 @@
 import sys, copy
 from collections import deque
+from itertools import combinations
 
 input = sys.stdin.readline
 
 
 def bfs():
     global ans
-    temp = copy.deepcopy(institute)
 
-    virus = deque([(x, y) for x in range(n) for y in range(m) if temp[x][y] == 2])
+    for wallCombi in combinations(empty, 3):
+        temp = copy.deepcopy(institute)
+        for wx, wy in wallCombi:
+            temp[wx][wy] = 1
 
-    while virus:
-        x, y = virus.popleft()
-        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            nx = x + dx
-            ny = y + dy
-            if 0 <= nx < n and 0 <= ny < m and temp[nx][ny] == 0:
-                temp[nx][ny] = 2
-                virus.append((nx, ny))
+        virus = deque([(x, y) for x in range(n) for y in range(m) if temp[x][y] == 2])
 
-    safe = 0
-    for line in temp:
-        safe += line.count(0)
-    ans = max(ans, safe)
+        while virus:
+            x, y = virus.popleft()
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx = x + dx
+                ny = y + dy
+                if 0 <= nx < n and 0 <= ny < m and temp[nx][ny] == 0:
+                    temp[nx][ny] = 2
+                    virus.append((nx, ny))
 
-
-def makeWall(cnt):
-    if cnt == 3:
-        bfs()
-        return
-
-    for i in range(n):
-        for j in range(m):
-            if institute[i][j] == 0:
-                institute[i][j] = 1
-                makeWall(cnt + 1)
-                institute[i][j] = 0
+        safe = 0
+        for line in temp:
+            safe += line.count(0)
+        ans = max(ans, safe)
 
 
 n, m = map(int, input().split())
 institute = [list(map(int, input().split())) for _ in range(n)]
+empty = [(x, y) for x in range(n) for y in range(m) if institute[x][y] == 0]
 ans = 0
-makeWall(0)
+bfs()
 print(ans)
