583cc87c6fdc3cba5281a8e972e47ac8d1536d13
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-10-16 10:43:13 +0900

    [Gold V] Title: 틱택토, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.py
+++ 백준/Gold/7682. 틱택토/틱택토.py
@@ -1,14 +1,14 @@
-def check_win(board, char):
+def check_win(mal):
     for i in range(3):
-        if all([board[i][j] == char for j in range(3)]) or all(
-            [board[j][i] == char for j in range(3)]
+        if all(board[i][j] == mal for j in range(3)) or all(
+            board[j][i] == mal for j in range(3)
         ):
             return True
-    if (
-        board[0][0] == board[1][1] == board[2][2] == char
-        or board[0][2] == board[1][1] == board[2][0] == char
-    ):
-        return True
+        if (
+            board[0][0] == board[1][1] == board[2][2] == mal
+            or board[0][2] == board[1][1] == board[2][0] == mal
+        ):
+            return True
     return False
 
 
@@ -21,18 +21,18 @@ while True:
     o_cnt = words.count('O')
     board = [list(words[i * 3 : i * 3 + 3]) for i in range(3)]
 
-    x_win = check_win(board, 'X')
-    o_win = check_win(board, 'O')
+    x_win = check_win('X')
+    o_win = check_win('O')
 
     if x_cnt < o_cnt or x_cnt > o_cnt + 1:
-        print("invalid")
+        print('invalid')
     elif (
         (x_win and x_cnt != o_cnt + 1)
         or (o_win and x_cnt != o_cnt)
         or (x_win and o_win)
     ):
-        print("invalid")
+        print('invalid')
     elif not x_win and not o_win and x_cnt + o_cnt != 9:
-        print("invalid")
+        print('invalid')
     else:
-        print("valid")
+        print('valid')
