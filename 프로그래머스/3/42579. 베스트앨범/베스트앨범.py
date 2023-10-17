7d7166d05e3df3182126153ffe74f6b3aee6fbca
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-10-17 13:56:07 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+
+def solution(genres, plays):
+    answer = []
+    music = defaultdict(list)
+    rank = defaultdict(int)
+    for idx, g in enumerate(genres):
+        music[g].append((plays[idx], idx))
+        rank[g] += plays[idx]
+    for key in music:
+        print(key)
+        music[key] = sorted(music[key], key=lambda x: x[0], reverse=True)
+    sorted_rank = sorted(rank.items(), key=lambda item: item[1], reverse=True)
+    for key, _ in sorted_rank:
+        for i in range(min(2, len(music[key]))):
+            answer.append(music[key][i][1])
+    return answer
