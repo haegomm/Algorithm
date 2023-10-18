206df0baea1ada414f33e5154e9f1106e4745c6d
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-10-19 01:07:29 +0900

    [Gold IV] Title: 샘터, Time: 196 ms, Memory: 56156 KB -BaekjoonHub

--- 백준/Gold/18513. 샘터/샘터.py
+++ 백준/Gold/18513. 샘터/샘터.py
@@ -1,29 +1,28 @@
-import sys
 from collections import deque
 
-n, k = map(int, sys.stdin.readline().split())
-n_list = list(map(int, sys.stdin.readline().split()))
-visit = set()
 
-dq = deque()
+def bfs():
+    global k
+    ans = 0
 
-for i in n_list:
-    dq.append((i, 1))
-    visit.add(i)
+    while k > 0:
+        now = deq.popleft()
+        for move in [now + 1, now - 1]:
+            if move not in visited and k > 0:
+                visited[move] = visited[now] + 1
+                ans += visited[move]
+                k -= 1
+                deq.append(move)
 
-result = 0
-now_build = 0
-while dq:
-    now, b = dq.popleft()
-    for d in [1, -1]:
-        nx = now + d
-        if nx in visit:
-            continue
-        visit.add(nx)
-        result += b
-        now_build += 1
-        dq.append((nx, b + 1))
-        if now_build == k:
-            dq = list()
-            break
-print(result)
+    return ans
+
+
+n, k = map(int, input().split())
+water = list(map(int, input().split()))
+deq = deque(water)
+visited = dict()
+
+for w in water:
+    visited[w] = 0
+
+print(bfs())
