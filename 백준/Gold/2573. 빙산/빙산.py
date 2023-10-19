ad60342e7fae1f2d7a208d5c9543cfddfcbe2548
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-10-19 13:29:40 +0900

    [Gold IV] Title: 빙산, Time: 4028 ms, Memory: 36624 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -5,54 +5,61 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
 
-def melt_ice():
-    melting_points = {}
-    for x, y in ice_points:
+def bfs():
+    melt = {}
+    for x, y in ice:
         cnt = 0
         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            nx, ny = x + dx, y + dy
+            nx = x + dx
+            ny = y + dy
             if 0 <= nx < n and 0 <= ny < m and polar[nx][ny] == 0:
                 cnt += 1
-        melting_points[(x, y)] = cnt
+        melt[(x, y)] = cnt
 
-    new_ice_points = set()
-    for x, y in ice_points:
-        polar[x][y] -= melting_points[(x, y)]
+    new = []
+    for x, y in ice:
+        polar[x][y] -= melt[(x, y)]
         if polar[x][y] < 0:
             polar[x][y] = 0
         elif polar[x][y] > 0:
-            new_ice_points.add((x, y))
+            new.append((x, y))
 
-    return new_ice_points
+    return new
 
 
 def dfs(x, y):
     visited[x][y] = True
     for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        nx, ny = x + dx, y + dy
-        if (nx, ny) in ice_points and not visited[nx][ny]:
+        nx = x + dx
+        ny = y + dy
+        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and polar[nx][ny] != 0:
             dfs(nx, ny)
 
 
 n, m = map(int, input().split())
 polar = [list(map(int, input().split())) for _ in range(n)]
-
-ice_points = {(i, j) for i in range(n) for j in range(m) if polar[i][j] > 0}
 time = 0
+ice = []
+
+for i in range(n):
+    for j in range(m):
+        if polar[i][j] != 0:
+            ice.append((i, j))
 
 while True:
     visited = [[False] * m for _ in range(n)]
-    components = 0
-    for x, y in list(ice_points):
-        if not visited[x][y]:
-            dfs(x, y)
-            components += 1
-            if components >= 2:
+    cnt = 0
+    for i, j in list(ice):
+        if not visited[i][j]:
+            dfs(i, j)
+            cnt += 1
+            if cnt >= 2:
                 print(time)
                 exit()
-    if not ice_points:
+
+    if not ice:
         print(0)
         break
 
-    ice_points = melt_ice()
+    ice = bfs()
     time += 1
