741db714f45e34c0c22238988d8e23fbf8324683
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-10-24 20:51:36 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 64 ms, Memory: 34104 KB -BaekjoonHub

--- 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.py
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.py
@@ -3,38 +3,43 @@ from collections import deque
 
 def move():
     global wall
-    visited = set()
+    visited = [[False] * 8 for _ in range(8)]
     deq = deque([(7, 0)])
 
     while deq:
         for _ in range(len(deq)):
             x, y = deq.popleft()
+            visited[x][y] = False
             if x == 0:
                 return 1
-            if (x, y) in wall:
+            if maze[x][y] == '#':
                 continue
             for dx, dy in [(0, 0), (-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
                 nx = x + dx
                 ny = y + dy
-                if 0 <= nx < 8 and 0 <= ny < 8 and not (nx, ny) in wall and not (nx, ny) in visited:
-                    visited.add((nx, ny))
+                if 0 <= nx < 8 and 0 <= ny < 8 and not visited[nx][ny] and maze[nx][ny] == '.':
+                    visited[nx][ny] = True
                     deq.append((nx, ny))
-        temp = set()
-        if wall:
-            visited = set()
+        temp = []
+        for wx, wy in wall[::-1]:
+            maze[wx][wy] = '.'
+            nwx = wx + 1
+            if nwx == 8:
+                continue
+            maze[nwx][wy] = '#'
+            temp.append((nwx, wy))
+        wall = temp[::-1]
         for wx, wy in wall:
-            if wx < 7:
-                temp.add((wx+1, wy))
-        wall = temp
+            visited[wx][wy] = False
     return 0
 
 
 maze = [list(input().strip()) for _ in range(8)]
-wall = set()
+wall = []
 
 for i in range(8):
     for j in range(8):
         if maze[i][j] == '#':
-            wall.add((i, j))
+            wall.append((i, j))
 
 print(move())
