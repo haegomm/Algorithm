811fc6a64d99e06116f9381beeffdd131fa3bce0
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-10-31 20:18:26 +0900

    [Silver V] Title: 왕복, Time: 76 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Silver/18311. 왕복/왕복.py
+++ 백준/Silver/18311. 왕복/왕복.py
@@ -1,29 +1,16 @@
+def solve():
+    now = 0
+    for i in range(n):
+        now += distance[i]
+        if now > k:
+            return i + 1
+
+    for i in range(n - 1, -1, -1):
+        now += distance[i]
+        if now > k:
+            return i + 1
+
 n, k = map(int, input().split())
 distance = list(map(int, input().split()))
-go = [0]
-back = []
 
-temp = 0
-for d in distance:
-    temp += d
-    go.append(temp)
-
-for d in distance[::-1]:
-    temp += d
-    back.append(temp)
-
-flag = False
-while True:
-    if not flag:
-        for i in range(n - 1):
-            if go[i] <= k < go[i + 1]:
-                print(i + 1)
-                exit()
-        else:
-            flag = True
-    else:
-        for i in range(n - 1):
-            if back[i] < k <= back[i + 1]:
-                print((n-1) - i)
-                break
-        break
+print(solve())
