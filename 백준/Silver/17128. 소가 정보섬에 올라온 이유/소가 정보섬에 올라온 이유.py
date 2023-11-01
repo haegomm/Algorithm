84cfdbc7e366afd1ff677e5429c83d81c54cb54d
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-11-01 17:44:06 +0900

    [Silver II] Title: 소가 정보섬에 올라온 이유, Time: 744 ms, Memory: 56352 KB -BaekjoonHub

---
+++ 백준/Silver/17128. 소가 정보섬에 올라온 이유/소가 정보섬에 올라온 이유.py
@@ -0,0 +1,16 @@
+n, q = map(int, input().split())
+cows = list(map(int, input().split()))
+sticker = list(map(int, input().split()))
+
+products = [(cows[i] * cows[(i+1)%n] * cows[(i+2)%n] * cows[(i+3)%n]) for i in range(n)]
+sum_S = sum(products)
+
+for i in sticker:
+    i -= 1
+    for j in range(4):
+        k = (i - j + n) % n
+        sum_S -= products[k]
+        products[k] *= -1
+        sum_S += products[k]
+
+    print(sum_S)
