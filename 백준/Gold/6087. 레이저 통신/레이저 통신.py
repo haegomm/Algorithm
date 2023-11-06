c6df98029b45441108d74587b43acfe5aaee12f0
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-11-06 11:03:04 +0900

    [Gold III] Title: 레이저 통신, Time: 88 ms, Memory: 34360 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.py
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.py
@@ -1,41 +1,50 @@
 from collections import deque
 
-W, H = map(int, input().split())
-grid = [list(input().strip()) for _ in range(H)]
+def bfs():
+    deq = deque() # (x, y, 방향, 거울 갯수)
 
-start_end = [(i, j) for i in range(H) for j in range(W) if grid[i][j] == 'C']
-start, end = start_end
+    # 초기 방향 설정
+    for d in range(4):
+        visited[start[0]][start[1]][d] = 0
+        deq.append((start[0], start[1], d, 0))
 
-directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    while deq:
+        x, y, dir, cnt = deq.popleft()
 
-visited = [[[float('inf')]*4 for _ in range(W)] for _ in range(H)]
+        if [x, y] == end:
+            return cnt
 
-queue = deque()
+        nx = x + dx[dir]
+        ny = y + dy[dir]
 
-for d in range(4):
-    visited[start[0]][start[1]][d] = 0
-    queue.append((start[0], start[1], d, 0))
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != '*':
 
-while queue:
-    x, y, dir, mirrors = queue.popleft()
-    
-    if (x, y) == end:
-        continue
+            # 직진하는 경우
+            if visited[nx][ny][dir] > cnt:
+                visited[nx][ny][dir] = cnt
+                deq.appendleft((nx, ny, dir, cnt))
 
-    dx, dy = directions[dir]
-    nx, ny = x + dx, y + dy
+            # 방향 전환하는 경우
+            for nd in range(4):
+                if nd != dir and visited[nx][ny][nd] > cnt + 1:
+                    visited[nx][ny][nd] = cnt + 1
+                    deq.append((nx, ny, nd, cnt + 1))
+        
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-    if 0 <= nx < H and 0 <= ny < W and grid[nx][ny] != '*':
+m, n = map(int, input().split())
+graph = [list(input().strip()) for _ in range(n)]
+visited = [[[float('inf')] * 4 for _ in range(m)] for _ in range(n)]
+start = []
+end = []
 
-        if visited[nx][ny][dir] > mirrors:
-            visited[nx][ny][dir] = mirrors
-            queue.appendleft((nx, ny, dir, mirrors))
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 'C':
+            if not start:
+                start = [i, j]
+                continue
+            end = [i, j]
 
-        for nd in range(4):
-            if nd != dir and visited[nx][ny][nd] > mirrors + 1:
-                visited[nx][ny][nd] = mirrors + 1
-                queue.append((nx, ny, nd, mirrors + 1))
-
-min_mirrors = min(visited[end[0]][end[1]])
-
-print(min_mirrors)
+print(bfs())
