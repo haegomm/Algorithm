ad2d707fb2fe9872af517c451e900615731d125a
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-11-07 14:14:22 +0900

    [Gold IV] Title: 탈출, Time: 68 ms, Memory: 34140 KB -BaekjoonHub

---
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -0,0 +1,51 @@
+from collections import deque
+
+def bfs():
+    global water
+    queue = deque([start])
+    visited[start[0]][start[1]] = 1
+
+    while queue:
+        
+        temp = []
+        for wx, wy in water:
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nwx = wx + dx
+                nwy = wy + dy
+                if 0 <= nwx < n and 0 <= nwy < m and forest[nwx][nwy] == '.':
+                    forest[nwx][nwy] = '*'
+                    temp.append((nwx, nwy))
+        water = temp
+
+        for _ in range(len(queue)):
+            x, y = queue.popleft()
+            if (x, y) == end:
+                return visited[x][y] - 1
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx = x + dx
+                ny = y + dy
+                if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0 and forest[nx][ny] != '*' and forest[nx][ny] != 'X':
+                    visited[nx][ny] = visited[x][y] + 1
+                    queue.append((nx, ny))
+    
+    return 'KAKTUS'
+
+
+n, m = map(int, input().split())
+forest = [list(input().strip()) for _ in range(n)]
+water = deque([])
+visited = [[0]*m for _ in range(n)]
+start = 0
+end = 0
+
+for i in range(n):
+    for j in range(m):
+        if forest[i][j] == 'S':
+            start = (i, j)
+            forest[i][j] = '.'
+        elif forest[i][j] == 'D':
+            end = (i, j)
+        elif forest[i][j] == '*':
+            water.append((i, j))
+
+print(bfs())
