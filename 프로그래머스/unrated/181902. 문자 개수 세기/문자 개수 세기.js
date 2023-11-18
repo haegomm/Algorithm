ad3b8505779138cbd5610c6b21bdcb516c1c436b
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2023-11-18 20:32:59 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,14 +1,12 @@
 function solution(my_string) {
     let answer = [];
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    answer.length = 52;
+    answer.fill(0);
+    
+    my_string.split("").map((word) => {
+        answer[alphabet.indexOf(word)] += 1;
+    })
 
-    for(let i = 65; i <= 90; i++){
-        const matches = my_string.match(new RegExp(String.fromCharCode(i), 'g')) || [];
-        answer.push(matches.length);
-    }
-
-    for(let i = 97; i <= 122; i++){
-        const matches = my_string.match(new RegExp(String.fromCharCode(i), 'g')) || [];
-        answer.push(matches.length);
-    }
     return answer;
 }
