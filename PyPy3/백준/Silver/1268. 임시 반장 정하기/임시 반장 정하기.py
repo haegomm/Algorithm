cdbfb74549a6d8418df1411658bdd91d0764c5d3
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2024-08-28 22:59:19 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 136 ms, Memory: 110948 KB -BaekjoonHub

--- PyPy3/백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
+++ PyPy3/백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -4,11 +4,11 @@ history = [list(map(int, input().split())) for _ in range(n)]
 chk = [0] * n
 
 for i in range(n):
-    for k in range(n):
-        if i != k:
+    for k in range(i + 1, n):
             for g in range(5):
                 if history[i][g] == history[k][g]:
                     chk[i] += 1
+                    chk[k] += 1
                     break
 
 max_chk = max(chk)
