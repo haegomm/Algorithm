ec165ad37a234acb3dac338e1bf441c4f86dba2d
LEEGAEUN <95625643+haegomm@users.noreply.github.com>
2025-01-13 22:46:43 +0900

    [Silver IV] Title: 암기왕, Time: 980 ms, Memory: 323736 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -1,7 +1,13 @@
-for _ in range(int(input())):
-    N = int(input())
-    li1 = set(map(int, input().split()))
-    M = int(input())
-    li2 = list(map(int, input().split()))
-    for n in li2:
-        print(1 if n in li1 else 0)
+tc = int(input())
+
+for t in range(tc):
+    n1 = int(input())
+    arr1 = set(map(int, input().split()))
+    n2 = int(input())
+    arr2 = list(map(int, input().split()))
+
+    for num in arr2:
+        if num in arr1:
+            print(1)
+        else:
+            print(0)
